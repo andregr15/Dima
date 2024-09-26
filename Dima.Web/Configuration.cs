1c562f4767d2b6f0650d1e2d65f8a6d6a2efc12d
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2024-09-26 11:32:41 -0300

    Adding MudThemeProvider, Light and Dark modes

---
+++ Dima.Web/Configuration.cs
@@ -0,0 +1,31 @@
+using MudBlazor;
+
+namespace Dima.Web;
+
+public static class Configuration
+{
+    public static MudTheme Theme =
+        new()
+        {
+            Typography = new() { Default = new Default { FontFamily = ["Raleway", "sans-serif"] } },
+            PaletteLight = new()
+            {
+                Primary = new("#1efa2d"),
+                Secondary = Colors.LightGreen.Darken3,
+                Background = Colors.Gray.Lighten4,
+                AppbarBackground = new("#1efa2d"),
+                AppbarText = Colors.Shades.Black,
+                TextPrimary = Colors.Shades.Black,
+                PrimaryContrastText = Colors.Shades.Black,
+                DrawerText = Colors.Shades.Black,
+                DrawerBackground = Colors.LightGreen.Lighten4
+            },
+            PaletteDark = new()
+            {
+                Primary = Colors.LightGreen.Accent3,
+                Secondary = Colors.LightGreen.Darken3,
+                AppbarBackground = Colors.LightGreen.Accent3,
+                AppbarText = Colors.Shades.Black,
+            }
+        };
+}
